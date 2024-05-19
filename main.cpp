8fdc5e52276534b6ccf65cfde82cff6b971a950c
Mina Omrani <mina@Minas-MacBook-Pro.local>
2024-05-19 05:17:56 +0330

    Revert the changes

--- main.cpp
+++ main.cpp
@@ -15,9 +15,6 @@ int main() {
     std::vector<double> Ax;
     std::vector<double> x(n, 1.0);
     std::vector<double> y(n, 0.0); 
-    std::vector<double> z(n, 0.0);
-    double a = 2.0; // Example scaling factor for z
-    double b = 3.0; // Example scaling factor for y
 
     read_mtx(filename,n, Ap, Ai, Ax);
         
@@ -26,23 +23,9 @@ int main() {
         x[i] = r;  
     }
 
-    std::cout << "Calling spmv_csr function..." << std::endl;
-    // Call the spmv_csr function
     double Stime = omp_get_wtime();
-    // sym_lib::spmv_csr(n, Ap.data(), Ai.data(), Ax.data(), x.data(), y.data(), z.data(), a, b);
-    // std:: cout <<"time taken by scaled csr is :"<< omp_get_wtime()-Stime;
-    sym_lib::spmv_csr_parallel(n, Ap.data(), Ai.data(), Ax.data(), x.data(), y.data());
-    std:: cout <<"time taken by parallel csr is :"<< omp_get_wtime()-Stime;
-
-    // Print the result
-    // std::cout << "Result of spmv_csr: ";
-    // for (int i = 0; i < rows; ++i) {
-    //     std::cout << z[i] << " ";
-    // }
-
-    // double Stime = omp_get_wtime();
-    // sym_lib::spmv_csr(n, Ap.data(), Ai.data(), Ax.data(), x.data(), y.data());
-    // std:: cout <<"time taken by csr is :"<< omp_get_wtime()-Stime;
+    sym_lib::spmv_csr(n, Ap.data(), Ai.data(), Ax.data(), x.data(), y.data());
+    std:: cout <<"time taken by csr is :"<< omp_get_wtime()-Stime;
 
     return 0;
  }
